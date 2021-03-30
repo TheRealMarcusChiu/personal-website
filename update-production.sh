18a00d1f156fdbbba2f2c4863cfd1be7390e7a7c
Marcus Chiu <marcuschiu9@gmail.com>
2021-03-30 16:37:32 -0500

    updated update-production.sh

--- update-production.sh
+++ update-production.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
-  cd personal-website
-  git pull origin master
+  rm -rf personal-website/
+  git clone https://github.com/TheRealMarcusChiu/personal-website.git
+  rm -rf personal-website/.git
 EOF
